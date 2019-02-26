589e89a4aa13372c2e3fb2ce9ee2505c4777cdbd
tar-xzvff <jv120228m@gmail.com>
2019-02-26 18:16:36 +0900

    認証関係の処理を追加

--- lib/auth.go
+++ lib/auth.go
@@ -2,6 +2,10 @@ package lib
 
 import (
 	"gopkg.in/resty.v1"
+	"os/user"
+	"log"
+	"os"
+	"io/ioutil"
 )
 
 func GetToken(memberId string, password string) string {
@@ -22,27 +26,86 @@ func GetToken(memberId string, password string) string {
 	return ""
 }
 
-func LogIn(memberId string, password string) bool {
-	if "" != GetToken(memberId, password) {
-		//	TODO	:	任意のディレクトリに取得したtokenを保存する処理
-		return true
-	} else {
+func SaveToken(token string) bool {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal( err )
 		return false
 	}
+
+	file, err := os.Create(usr.HomeDir + "/.sakura_vps_token")
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	output := token
+	file.Write(([]byte)(output))
+	return true
+}
+
+func LoadToken() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal( err )
+	}
+
+	dat, err := ioutil.ReadFile(usr.HomeDir + "/.sakura_vps_token")
+	if err != nil {
+		log.Fatal( err )
+	}
+
+	return string(dat)
+}
+
+
+func ClearToken() bool {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal( err )
+		return false
+	}
+
+	file, err := os.Create(usr.HomeDir + "/.sakura_vps_token")
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	output := ""
+	file.Write(([]byte)(output))
+	return true
+}
+
+func LogIn(memberId string, password string) bool {
+	token := GetToken(memberId, password)
+	if "" != token {
+		if SaveToken(token) {
+			return true
+		}
+	}
+	return false
 }
 
 func LogOut() bool {
 	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/auth/logout"
+	var token = LoadToken()
+	if token == "" {
+		return false
+	}
+
 	resp, _ := resty.R().
 		SetHeaders(map[string]string{
 			"X-Requested-With": "XMLHttpRequest",
 			"Host": "secure.sakura.ad.jp",
 			"Origin": "https://secure.sakura.ad.jp",
 			"Content-Type": "application/json",
-			"Cookie": "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7",
+			"Cookie": token,
 		}).
 		Post(url)
+
 	if resp.StatusCode() == 200 {
+		ClearToken()
 		return true
 	} else {
 		return false
