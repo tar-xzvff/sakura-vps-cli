66893232491b0154b6998ae5c79acd3f288ec3cf
tar-xzvff <jv120228m@gmail.com>
2019-02-26 18:30:11 +0900

    サーバの電源操作処理を追加

--- model/object/server.go
+++ model/object/server.go
@@ -1,5 +1,11 @@
 package object
 
+import (
+	"fmt"
+	"gopkg.in/resty.v1"
+	"strconv"
+)
+
 type Server struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
@@ -38,4 +44,98 @@ type Server struct {
 	Limitation int    `json:"limitation"`
 	Version    string `json:"version"`
 	DNSRecords int    `json:"dns_records"`
+}
+
+func (t *Server) getPower() string {
+	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/powers/" + strconv.Itoa(t.ID)
+	resp, _ := resty.R().
+		SetHeader("Cookie", "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7").
+		Get(url)
+	fmt.Println(url)
+	fmt.Println(resp)
+	return "on"
+}
+
+func (t *Server) PowerOn() bool {
+	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/powers/" + strconv.Itoa(t.ID) + "/start"
+	resp, _ := resty.R().
+		SetHeaders(map[string]string{
+			"X-Requested-With": "XMLHttpRequest",
+			"Host": "secure.sakura.ad.jp",
+			"Origin": "https://secure.sakura.ad.jp",
+			"Content-Type": "application/json",
+			"Cookie": "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7",
+		}).
+		Post(url)
+	fmt.Println(url)
+	if resp.StatusCode() == 202 {
+		t.getPower()
+		return true
+	} else {
+		fmt.Println(resp.RawResponse)
+		return false
+	}
+}
+
+func (t *Server) PowerOff() bool {
+	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/powers/" + strconv.Itoa(t.ID) + "/destroy"
+	resp, _ := resty.R().
+		SetHeaders(map[string]string{
+			"X-Requested-With": "XMLHttpRequest",
+			"Host": "secure.sakura.ad.jp",
+			"Origin": "https://secure.sakura.ad.jp",
+			"Content-Type": "application/json",
+			"Cookie": "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7",
+		}).
+		Post(url)
+	fmt.Println(url)
+	if resp.StatusCode() == 202 {
+		t.getPower()
+		return true
+	} else {
+		fmt.Println(resp.RawResponse)
+		return false
+	}
+}
+
+func (t *Server) Shutdown() bool {
+	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/powers/" + strconv.Itoa(t.ID) + "/shutdown"
+	resp, _ := resty.R().
+		SetHeaders(map[string]string{
+			"X-Requested-With": "XMLHttpRequest",
+			"Host": "secure.sakura.ad.jp",
+			"Origin": "https://secure.sakura.ad.jp",
+			"Content-Type": "application/json",
+			"Cookie": "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7",
+		}).
+		Post(url)
+	fmt.Println(url)
+	if resp.StatusCode() == 202 {
+		t.getPower()
+		return true
+	} else {
+		fmt.Println(resp.RawResponse)
+		return false
+	}
+}
+
+func (t *Server) Reset() bool {
+	var url = "https://secure.sakura.ad.jp/vps/api/v6.5/powers/" + strconv.Itoa(t.ID) + "/reset"
+	resp, _ := resty.R().
+		SetHeaders(map[string]string{
+			"X-Requested-With": "XMLHttpRequest",
+			"Host": "secure.sakura.ad.jp",
+			"Origin": "https://secure.sakura.ad.jp",
+			"Content-Type": "application/json",
+			"Cookie": "SIDv2=e0b0880e-9859-49ee-a07c-3cd2c8d0e6d7",
+		}).
+		Post(url)
+	fmt.Println(url)
+	if resp.StatusCode() == 202 {
+		t.getPower()
+		return true
+	} else {
+		fmt.Println(resp.RawResponse)
+		return false
+	}
 }
